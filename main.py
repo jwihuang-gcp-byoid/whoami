427ef3eda9b315abe5b9bb3930a4c40e32c0eff6
jiawei huang <jwihuang@google.com>
2025-03-26 14:11:48 -0700

    change the default listening port

--- main.py
+++ main.py
@@ -42,5 +42,5 @@ async def home(request: Request):
 if __name__ == "__main__":
     import os
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 443))
     uvicorn.run(app, host="0.0.0.0", port=port)
